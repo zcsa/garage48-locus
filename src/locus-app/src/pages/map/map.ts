2bf9549d1f58fb49409bdcc37486eb5f0f58f226
Kristjan Toots <ktoots@tlu.ee>
2017-06-11 11:44:47 +0300

    Map selected on click

--- src/locus-app/src/pages/map/map.ts
+++ src/locus-app/src/pages/map/map.ts
@@ -15,11 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
+  public isBackgroundSelected = true;
+  public isMapSelected = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+  }
+
+  changeToSelected() {
+    console.log("hello");
+    this.isBackgroundSelected = false;
+    this.isMapSelected = true;
+    // this.navCtrl.parent.select(2);
+
   }
 
 }
